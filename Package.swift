1ee22608fe2ea026fd300c5dbe7eef624296779e
Robert Burns <burnsra@gmail.com>
2021-10-19 16:05:02 -0400

    Fix

--- Package.swift
+++ Package.swift
@@ -23,9 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NEKit-macOS",
-            dependencies: []),
-        .testTarget(
-            name: "NEKitTests",
-            dependencies: ["NEKit"]),
+            dependencies: [])
     ]
 )
