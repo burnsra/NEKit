9c3e6c6f85cbddff065ea46a7d94ccbccd67a203
Robert Burns <burnsra@gmail.com>
2021-10-20 08:30:14 -0400

    Revert "Fix"
    This reverts commit 1ee22608fe2ea026fd300c5dbe7eef624296779e.

--- Package.swift
+++ Package.swift
@@ -23,6 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NEKit-macOS",
-            dependencies: [])
+            dependencies: []),
+        .testTarget(
+            name: "NEKitTests",
+            dependencies: ["NEKit"]),
     ]
 )
