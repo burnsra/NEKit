dd80f52d1cadc30010b536c8fdb26c605674457b
Robert Burns <burnsra@gmail.com>
2024-07-29 14:15:13 -0400

    Updates for SPM

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "NEKit",
     platforms: [
-        .macOS(.v11)
+        .macOS(.v10_13)
     ],
     products: [
         .library(
@@ -15,19 +15,17 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", from: "7.6.5"),
         .package(url: "https://github.com/zhuhaow/tun2socks.git", from: "0.8.0"),
-        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack", from: "3.8.0"),
-        .package(url: "https://github.com/zhuhaow/Resolver", from: "0.3.0")
+        .package(url: "https://github.com/zhuhaow/Resolver", from: "0.3.0"),
+        .package(name: "Sodium", url: "https://github.com/jedisct1/swift-sodium", from: "0.9.1")
     ],
     targets: [
         .target(
             name: "NEKit",
             dependencies: [
                 .product(name: "CocoaAsyncSocket", package: "CocoaAsyncSocket"),
-                .product(name: "CocoaLumberjack", package: "CocoaLumberjack"),
-                .product(name: "CocoaLumberjackSwift", package: "CocoaLumberjack"),
-                .product(name: "CocoaLumberjackSwiftLogBackend", package: "CocoaLumberjack"),
                 .product(name: "tun2socks", package: "tun2socks"),
-                .product(name: "Resolver", package: "Resolver")
+                .product(name: "Resolver", package: "Resolver"),
+                .product(name: "Clibsodium", package: "Sodium")
             ],
             path: "src"
         ),
